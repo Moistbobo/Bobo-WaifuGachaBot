b9284cea93965d4249e55413a7fa7bc98b332aba
moistbobo <moistbobo@outlook.com>
2020-01-26 14:27:29 +0800

    fix(HelloWorld): Add missing description

--- src/commands/HelloWorld/index.ts
+++ src/commands/HelloWorld/index.ts
@@ -4,6 +4,7 @@ import { ICommand } from '../../models/ICommand';
 const HelloWorld: ICommand = {
   name: 'Hello world',
   triggers: ['helloWorld', 'hw'],
+  description: 'Hello world!',
   action,
 };
 
