339fa331f223e80f39d7238016ecedb3f5740380
Kevin <moistbobo@outlook.com>
2020-02-08 17:45:55 +0800

    feat: whitelist server for ssh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+user=${BOBO_WAIFUBOT_USER}
 host=${BOBO_WAIFUBOT_HOST}
 path=${BOBO_WAIFUBOT_PATH}
-echo "HOST:" + ${host}
+echo "HOST:" + $user}@${host}
 echo "PATH:" + ${path}
 
-scp -r package.json .env dist/** ${host}:${path}
+ssh-keyscan ${host} >> ~/.ssh/known_hosts
+scp -r package.json .env dist/** ${user}@${host}:${path}
 ssh ${host} 'cd test; npm i; exit;'
